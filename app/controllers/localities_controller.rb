d17d869f40cdc54eb74a92b92f8ad7c30526514c
César Amiconi <cesar.amiconi@gmail.com>
2023-01-09 22:41:02 -0300

    La base del CRUD de Localidades

--- app/controllers/localities_controller.rb
+++ app/controllers/localities_controller.rb
@@ -1,4 +1,47 @@
 class LocalitiesController < ApplicationController
+    
     def new
-    end    
+        @locality = Locality.new
+    end
+
+    def index
+       @localities = Locality.all 
+    end
+       
+    def create
+        @locality = Locality.new locality_params
+        @locality.save
+
+        redirect_to @locality
+    end
+
+    def edit
+        @locality = Locality.find params[:id]
+    end
+
+    def update
+        @locality = Locality.find params[:id]
+        @locality.update locality_params
+
+        redirect_to @locality
+    end
+
+    def destroy
+        @locality = Locality.find params[:id]
+        @locality.destroy
+
+        redirect_to localities_path
+    end
+
+
+    def show
+        @locality = Locality.find params[:id]
+    end
+
+    private
+
+        def locality_params
+            params.require(:locality).permit(:name, :province)
+        end
+
 end
