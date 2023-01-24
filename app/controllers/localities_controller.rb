3a852a74b2bb6a1f31aeb0bb011d6ba6797516c7
César Amiconi <cesar.amiconi@gmail.com>
2023-01-24 19:55:01 -0300

    Se agrego boostrap al Crud de localities

--- app/controllers/localities_controller.rb
+++ app/controllers/localities_controller.rb
@@ -25,7 +25,7 @@ class LocalitiesController < ApplicationController
 
     respond_to do |format|
       if @locality.save
-        format.html { redirect_to locality_url(@locality), notice: "Locality was successfully created." }
+        format.html { redirect_to locality_url(@locality), notice: "Localidad agregada exitosamente" }
         format.json { render :show, status: :created, location: @locality }
       else
         format.html { render :new, status: :unprocessable_entity }
@@ -38,7 +38,7 @@ class LocalitiesController < ApplicationController
   def update
     respond_to do |format|
       if @locality.update(locality_params)
-        format.html { redirect_to locality_url(@locality), notice: "Locality was successfully updated." }
+        format.html { redirect_to locality_url(@locality), notice: "Localidad actualizada exitosamente" }
         format.json { render :show, status: :ok, location: @locality }
       else
         format.html { render :edit, status: :unprocessable_entity }
@@ -52,7 +52,7 @@ class LocalitiesController < ApplicationController
     @locality.destroy
 
     respond_to do |format|
-      format.html { redirect_to localities_url, notice: "Locality was successfully destroyed." }
+      format.html { redirect_to localities_url, notice: "Localidad eliminada exitosamente" }
       format.json { head :no_content }
     end
   end
