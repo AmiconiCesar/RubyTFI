ca64639c021705cca34abe92b0298203b6a7d2b7
César Amiconi <cesar.amiconi@gmail.com>
2023-01-30 19:10:26 -0300

    Se modifico la estructura del modelo Schedule y se modifico MVC del mismo

--- app/models/schedule.rb
+++ app/models/schedule.rb
@@ -1,4 +1,9 @@
 class Schedule < ApplicationRecord
     validates :weekday, :start_time, :end_time,  presence: { message: "No puede ser nulo"}
-    validates :weekday, uniqueness: { message: "Ya existe este día de la semana"}
+    validates :weekday, uniqueness: { message: "Ya está registrado"}
+    validates :start_time, comparison: { less_than: :end_time , 
+        message: "debe ser anterior al Horario hasta" }
+    
+    enum weekday: [:Domingo, :Lunes, :Martes, :Miércoles, :Jueves, :Viernes, :Sábado]    
+
 end
