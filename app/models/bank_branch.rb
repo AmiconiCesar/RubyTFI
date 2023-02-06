f15b5158c1c703b76e006a6e5c19e1c71e2511bf
AmiconiCesar <101070571+AmiconiCesar@users.noreply.github.com>
2023-02-06 09:01:48 -0300

    fix (#8)
    * Se crea el modelo Bank_branch y primeras validaciones
    
    * Se agregaron relaciones de Branches con Schedule y con Locality, migration y validates
    
    * Se acomodaron las vistas de Branches y la asociacion con Locality
    
    * Se modifico el show individual de una Branch
    
    * Se cambiaron las vistas de los schedules asociados a los branch, se arreglaron algunas navegaciones
    
    * Se agrego el borrado en cascada para los Branches
    
    * Algunos fix en Branches
    
    ---------
    
    Co-authored-by: César Amiconi <cesar.amiconi@gmail.com>

--- app/models/bank_branch.rb
+++ app/models/bank_branch.rb
@@ -2,8 +2,8 @@ class BankBranch < ApplicationRecord
     validates :name, :address, :phone, presence: { message: "No puede ser nulo"}
     validates :name, uniqueness: { message: "Ya existe esta Sucursal" }
     belongs_to :locality
-    has_many :schedules dependent: :destroy
-    has_many :turns dependent: :destroy
+    has_many :schedules, dependent: :destroy
+    has_many :turns, dependent: :destroy
     
     def to_string
         "#{name.capitalize} - #{locality.to_string}"
