3d1e9f7d7d6626fe05df77e3bc4d49c4a5cd5f02
César Amiconi <cesar.amiconi@gmail.com>
2023-02-06 16:56:48 -0300

    Se agrego login,logout y manejo de sessions, y pagina home base

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,14 @@
 class ApplicationController < ActionController::Base
+    before_action :set_current_user
+    before_action :protected_pages
+
+
+    def set_current_user
+        Current.user = User.find_by(id: session[:user_id]) if session[:user_id]
+    end    
+
+    def protected_pages
+        redirect_to new_session_path, alert: "Debe loguearse" unless Current.user
+    end
+        
 end
